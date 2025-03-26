71260b052e841508da74481c3655a446f03f6e7e
xxchinitoxpz <brayanhornamartinez@gmail.com>
2025-03-26 01:54:24 -0500

    awa

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -6,37 +6,42 @@
 
 
 
+
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3">
-                        id
+                        ID
                     </th>
                     <th scope="col" class="px-6 py-3">
                         Category
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Action
+                        <span class="sr-only">Edit</span>
                     </th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($categories as $category)
                     <tr
-                        class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                     
-                        <th scope="col" class="px-6 py-3">
-                            {{$category->id}}
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <th scope="row"
+                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $category->id }}
                         </th>
-                        <th scope="col" class="px-6 py-3">
-                            {{$category->name}}
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            {{$category->id}}
-                        </th>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $category->name }}
+                        </td>
+                        <td class="px-6 py-4 text-right">
+                            <a href="#"
+                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
                     </tr>
                 @endforeach
+
+
+
 
             </tbody>
         </table>
